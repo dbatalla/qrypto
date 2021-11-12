6f19be29f3ce9be11e11e429b0de2d9c88278a6c
Damian Batalla <dbatalla@gmail.com>
2021-11-11 21:49:05 -0300

    Initial commit

---
+++ src/main/kotlin/com/quwisoft/qrypto/QryptoApplication.kt
@@ -0,0 +1,11 @@
+package com.quwisoft.qrypto
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class QryptoApplication
+
+fun main(args: Array<String>) {
+    runApplication<QryptoApplication>(*args)
+}
